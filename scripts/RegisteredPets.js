73a2a0b96a6eea14a1872e75e68d9cdc656dd286
Hannah Thompson <hannahmthompsonedu@gmail.com>
2023-09-18 14:35:44 -0400

    standby

--- scripts/RegisteredPets.js
+++ scripts/RegisteredPets.js
@@ -1,12 +1,40 @@
-import { getPets } from "./database.js"
+import {getPets} from "./database.js"
 
 const pets = getPets()
+
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const itemClicked = clickEvent.target 
+
+        if (itemClicked.id.startsWith("pet")) {
+
+            const [,petPairs] = itemClicked.id.split("--")
+
+            let matchingPet = null
+            for (const pet of pets) {
+                if (parseInt(petPairs) === pet.id) {
+                    matchingPet = pet
+                }
+            }
+
+            let matchingWalker = null
+            for (const walker of walkers) {
+                if (matchingPet.walkerId === walker.Id) {
+                    matchingWalker = walker
+                }
+            }
+
+            window.alert(`${matchingPet.name} is being walked by ${matchingWalker.name}`)
+        }
+    }
+) 
 
 export const RegisteredPets = () => {
     let petHTML = "<ul>"
 
     for (const pet of pets) {
-        petHTML += `<li>${pet.name}</li>`
+        petHTML += `<li id="pet--${pet.id}">${pet.name}</li>`
     }
 
     petHTML += "</ul>"
